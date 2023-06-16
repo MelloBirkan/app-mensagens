67f4f48d0180261322dd09c02e86e08260f6e822
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-16 13:40:17 -0300

    Initial Commit

---
+++ Mensagens/MensagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensagensApp.swift
+//  Mensagens
+//
+//  Created by Marcello Gonzatto Birkan on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MensagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
