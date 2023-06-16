a7a4468433200ecd537f251dd39f0db81cc4c4d7
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-16 13:52:00 -0300

    inicio do esqueleto do projeto

---
+++ Mensagens/Views/ConversationListView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Mensagens
+//
+//  Created by Marcello Gonzatto Birkan on 16/06/23.
+//
+
+import SwiftUI
+
+struct ConversationListView: View {
+    var body: some View {
+        Text("i")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationListView()
+    }
+}
