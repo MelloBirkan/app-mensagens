f10c8f86a6efebdd76c0aab999d4f2d27b819088
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-16 14:49:49 -0300

    Escopos e Imagens adicionadas

---
+++ Mensagens/Models/AppStateModel.swift
@@ -0,0 +1,8 @@
+//
+//  AppStateModel.swift
+//  Mensagens
+//
+//  Created by Marcello Gonzatto Birkan on 16/06/23.
+//
+
+import Foundation
---
+++ Mensagens/Views/SendButton.swift
@@ -0,0 +1,20 @@
+//
+//  SendButton.swift
+//  Mensagens
+//
+//  Created by Marcello Gonzatto Birkan on 16/06/23.
+//
+
+import SwiftUI
+
+struct SendButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SendButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SendButton()
+    }
+}
